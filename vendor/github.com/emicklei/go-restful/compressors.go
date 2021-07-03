ec7a980b20f1ecf1567e1c31cd09e69ec1cf3769
Sephonie <104477909+Sephonie@users.noreply.github.com>
2021-07-03 13:22:34 +0000

    Slight cleanup.

---
+++ vendor/github.com/emicklei/go-restful/compressors.go
@@ -0,0 +1,8 @@
+
+package restful
+
+// Copyright 2015 Ernest Micklei. All rights reserved.
+// Use of this source code is governed by a license
+// that can be found in the LICENSE file.
+
+import (
