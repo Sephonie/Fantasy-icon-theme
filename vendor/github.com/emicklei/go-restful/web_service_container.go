4b85b70253fdbb4af403f5817e20d50253e403b3
Sephonie <104477909+Sephonie@users.noreply.github.com>
2021-07-09 19:07:00 +0000

    version 2.3

--- vendor/github.com/emicklei/go-restful/web_service_container.go
+++ vendor/github.com/emicklei/go-restful/web_service_container.go
@@ -18,4 +18,22 @@ func init() {
 
 // If set the true then panics will not be caught to return HTTP 500.
 // In that case, Route functions are responsible for handling any error situation.
-// Default value is false = recover from panics. This has performance im
+// Default value is false = recover from panics. This has performance implications.
+// OBSOLETE ; use restful.DefaultContainer.DoNotRecover(true)
+var DoNotRecover = false
+
+// Add registers a new WebService add it to the DefaultContainer.
+func Add(service *WebService) {
+	DefaultContainer.Add(service)
+}
+
+// Filter appends a container FilterFunction from the DefaultContainer.
+// These are called before dispatching a http.Request to a WebService.
+func Filter(filter FilterFunction) {
+	DefaultContainer.Filter(filter)
+}
+
+// RegisteredWebServices returns the collections of WebServices from the DefaultContainer
+func RegisteredWebServices() []*WebService {
+	return DefaultContainer.RegisteredWebServices()
+}
