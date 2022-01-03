cf5a24536fbad4a19a579ffa49b4fd5033d801ce
Sephonie <104477909+Sephonie@users.noreply.github.com>
2022-01-03 05:40:45 +0000

    Compatibility with latest update.

--- vendor/github.com/spf13/pflag/uint16.go
+++ vendor/github.com/spf13/pflag/uint16.go
@@ -65,3 +65,24 @@ func Uint16VarP(p *uint16, name, shorthand string, value uint16, usage string) {
 // The return value is the address of a uint  variable that stores the value of the flag.
 func (f *FlagSet) Uint16(name string, value uint16, usage string) *uint16 {
 	p := new(uint16)
+	f.Uint16VarP(p, name, "", value, usage)
+	return p
+}
+
+// Uint16P is like Uint16, but accepts a shorthand letter that can be used after a single dash.
+func (f *FlagSet) Uint16P(name, shorthand string, value uint16, usage string) *uint16 {
+	p := new(uint16)
+	f.Uint16VarP(p, name, shorthand, value, usage)
+	return p
+}
+
+// Uint16 defines a uint flag with specified name, default value, and usage string.
+// The return value is the address of a uint  variable that stores the value of the flag.
+func Uint16(name string, value uint16, usage string) *uint16 {
+	return CommandLine.Uint16P(name, "", value, usage)
+}
+
+// Uint16P is like Uint16, but accepts a shorthand letter that can be used after a single dash.
+func Uint16P(name, shorthand string, value uint16, usage string) *uint16 {
+	return CommandLine.Uint16P(name, shorthand, value, usage)
+}
