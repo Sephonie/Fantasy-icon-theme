344b2266f11d6c7d578c3cfc96a6587a8ab7c6b9
Sephonie <104477909+Sephonie@users.noreply.github.com>
2022-01-25 21:25:15 +0000

    Conflicts resolved with rel-v6r12

--- vendor/github.com/stretchr/testify/assert/forward_assertions.go
+++ vendor/github.com/stretchr/testify/assert/forward_assertions.go
@@ -7,4 +7,10 @@ type Assertions struct {
 }
 
 // New makes a new Assertions object for the specified TestingT.
-func New(t TestingT)
+func New(t TestingT) *Assertions {
+	return &Assertions{
+		t: t,
+	}
+}
+
+//go:generate go run ../_codegen/main.go -output-package=assert -template=assertion_forward.go.tmpl -include-format-funcs
