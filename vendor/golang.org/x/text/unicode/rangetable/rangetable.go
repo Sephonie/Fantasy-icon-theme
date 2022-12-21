a0e79be288cfacb3151c783998320b77149699d7
Sephonie <104477909+Sephonie@users.noreply.github.com>
2022-12-21 16:10:28 +0000

    Add test file

--- vendor/golang.org/x/text/unicode/rangetable/rangetable.go
+++ vendor/golang.org/x/text/unicode/rangetable/rangetable.go
@@ -38,4 +38,33 @@ func New(r ...rune) *unicode.RangeTable {
 	}
 
 	// Optimize RangeTable.
-	r
+	return Merge(&rt)
+}
+
+type byRune []rune
+
+func (r byRune) Len() int           { return len(r) }
+func (r byRune) Swap(i, j int)      { r[i], r[j] = r[j], r[i] }
+func (r byRune) Less(i, j int) bool { return r[i] < r[j] }
+
+// Visit visits all runes in the given RangeTable in order, calling fn for each.
+func Visit(rt *unicode.RangeTable, fn func(rune)) {
+	for _, r16 := range rt.R16 {
+		for r := rune(r16.Lo); r <= rune(r16.Hi); r += rune(r16.Stride) {
+			fn(r)
+		}
+	}
+	for _, r32 := range rt.R32 {
+		for r := rune(r32.Lo); r <= rune(r32.Hi); r += rune(r32.Stride) {
+			fn(r)
+		}
+	}
+}
+
+// Assigned returns a RangeTable with all assigned code points for a given
+// Unicode version. This includes graphic, format, control, and private-use
+// characters. It returns nil if the data for the given version is not
+// available.
+func Assigned(version string) *unicode.RangeTable {
+	return assigned[version]
+}
