9f8c0f07bc4293c92dd39050ecbf6d326479e971
Sephonie <104477909+Sephonie@users.noreply.github.com>
2023-01-07 12:07:49 +0000

    attempt to fix build

--- vendor/k8s.io/apimachinery/pkg/apis/meta/v1/doc.go
+++ vendor/k8s.io/apimachinery/pkg/apis/meta/v1/doc.go
@@ -10,4 +10,13 @@ You may obtain a copy of the License at
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions an
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// +k8s:deepcopy-gen=package
+// +k8s:openapi-gen=true
+// +k8s:defaulter-gen=TypeMeta
+
+// +groupName=meta.k8s.io
+package v1 // import "k8s.io/apimachinery/pkg/apis/meta/v1"
