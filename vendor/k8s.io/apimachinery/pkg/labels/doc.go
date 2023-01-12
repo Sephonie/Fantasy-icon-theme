1e574b0dd5cad90d1c6b22ac3d3f5f7485b14eb1
Sephonie <104477909+Sephonie@users.noreply.github.com>
2023-01-12 16:48:09 +0000

    Remove logger

--- vendor/k8s.io/apimachinery/pkg/labels/doc.go
+++ vendor/k8s.io/apimachinery/pkg/labels/doc.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2016 The Kubernetes Authors.
+Copyright 2014 The Kubernetes Authors.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,17 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-package fields
-
-import "k8s.io/apimachinery/pkg/selection"
-
-// Requirements is AND of all requirements.
-type Requirements []Requirement
-
-// Requirement contains a field, a value, and an operator that relates the field and value.
-// This is currently for reading internal selection information of field selector.
-type Requirement struct {
-	Operator selection.Operator
-	Field    string
-	Value    string
-}
+// Package labels implements a simple label system, parsing and matching
+// selectors with sets of labels.
+package labels // import "k8s.io/apimachinery/pkg/labels"
