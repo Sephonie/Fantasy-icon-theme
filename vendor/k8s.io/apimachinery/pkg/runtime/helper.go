887b80bacf3dbb47283d9a2261f47bd4f8c03f92
Sephonie <104477909+Sephonie@users.noreply.github.com>
2023-01-19 20:13:39 +0000

    Typos fixed.

--- vendor/k8s.io/apimachinery/pkg/runtime/helper.go
+++ vendor/k8s.io/apimachinery/pkg/runtime/helper.go
@@ -209,4 +209,4 @@ var DefaultFramer = defaultFramer{}
 type defaultFramer struct{}
 
 func (defaultFramer) NewFrameReader(r io.ReadCloser) io.ReadCloser { return r }
-func (defaultFramer) NewFrameWriter(w io.Writer) io.Writer         {
+func (defaultFramer) NewFrameWriter(w io.Writer) io.Writer         { return w }
